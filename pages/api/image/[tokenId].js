51a250e63a4c4d553322a6a0d109fd07b85af8a5
NuclearGeek <spickett84@yahoo.com>
2023-10-15 01:14:42 -0400

    Change image api to sharp. difficult to find something compatible with next.js

--- pages/api/image/[tokenId].js
+++ pages/api/image/[tokenId].js
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 import ABI from '../abi.json';
-import Jimp from 'jimp';
-import path from "path";
+import sharp from 'sharp';
 
 export default async function handler(req, res) {
   const { tokenId } = req.query;
@@ -26,21 +25,16 @@ export default async function handler(req, res) {
     }],
   };
 
-  const image = new Jimp(500, 500, 0xffffffff);
-  // const plugin = require.resolve('@jimp/plugin-print');
-  // const jimpFont = path.resolve(plugin, '../../../fonts/open-sans/open-sans-32-black/open-sans-32-black.fnt');
-  // const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK); // use your desired font
-  const jimpFont = path.resolve(Jimp.FONT_SANS_32_BLACK);
-  const font = await Jimp.loadFont(jimpFont);
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500">
+    <text x="10" y="20">${baseJson.name}</text>
+    <text x="10" y="40">${baseJson.description}</text>
+    <text x="10" y="60">${baseJson.attributes[0].trait_type}: ${baseJson.attributes[0].value}</text>
+  </svg>`;
 
-  image.print(font, 10, 10, baseJson.name);
-  image.print(font, 10, 50, baseJson.description);
-  image.print(font, 10, 90, baseJson.attributes[0].trait_type);
-  image.print(font, 10, 130, baseJson.attributes[0].value);
+  // Convert SVG to PNG with Sharp
+  const buffer = await sharp(Buffer.from(svgString)).png().toBuffer();
 
-  const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
-
+  // Set the Content Type and send the image
   res.setHeader('Content-Type', 'image/png');
   res.send(buffer);
-
 }
